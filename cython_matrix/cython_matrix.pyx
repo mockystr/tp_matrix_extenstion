14ba169aabb6fde02cf6cdeb3797ca4ea88716ec
Emir <navruzov.e1511@gmail.com>
2019-05-11 15:37:44 +0300

    cython double[:, :] error

--- cython_matrix/cython_matrix.pyx
+++ cython_matrix/cython_matrix.pyx
@@ -1,22 +1,24 @@
 import random
-from pprint import pprint
 
-numbers_list = [1.23, 5.123, 5.7, 5, 6, 2, 1.6, 0.7, 5.5, 8.1, 4.9, 1232, 1000, 123123123,
-            34234234.52, 5456778.13, 76.234,677, 763.5]
+cdef list numbers_list = [1.23, 5.123, 5.7, 5, 6, 2, 1.6, 0.7, 5.5, 8.1, 4.9, 
+                        1232, 1000, 123123123, 34234234.52, 5456778.13, 76.234,677, 763.5]
 
 
-def matrix_mul(m1, m2):
-    cdef list r = [[0] * len(m2[0]) for _ in range(len(m1))]
+cpdef double[:,:] matrix_mul(double[:,:] m1, double[:, :] m2):
+    cdef int _=0
+    cdef int i=0, j=0, k=0 
+    cdef double[:, :] r = [[0] * len(m2[0]) for _ in range(len(m1))]
+
     for i in range(len(m1)):
         for j in range(len(m2[0])):
             for k in range(len(m1[0])):
                 r[i][j] += m1[i][k] * m2[k][j]
-
     return r
 
 
-def gen_matrix(int n):
-    cdef list ll = []
+cpdef double[:,:] gen_matrix(int n):
+    cdef int _, i=0
+    cdef double[:, :] ll
     for _ in range(n):
-        ll.append([random.choice(numbers_list) for i in range(n)])
+        ll[_] = [random.choice(numbers_list) for i in range(n)]
     return ll
